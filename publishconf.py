8cd8d2001d3992af1de4cefde084613458571e6a
Amit Chaudhary <studenton.com@gmail.com>
2016-10-15 18:24:52 +0545

    Add deploy configuration

---
+++ publishconf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+import os
+import sys
+
+sys.path.append(os.curdir)
+
+from pelicanconf import *
+
+# Deployment Settings
+SITEURL = 'https://studenton.com'
+RELATIVE_URLS = False
+USE_LESS = False
+GOOGLE_ANALYTICS = "UA-35734550-1"
+
+# Delete the output directory, before generating new files
+DELETE_OUTPUT_DIRECTORY = True
